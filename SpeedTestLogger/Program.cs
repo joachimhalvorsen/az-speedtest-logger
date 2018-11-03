b808044e5ab8a399dc683c20b16e2b2cf560ee44
Joachim Halvorsen <joh@computas.com>
2018-11-03 12:29:20 +0100

    SpeedTestLogger first version, writing to console

---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using SpeedTest;
+using SpeedTestLogger.Models;
+
+namespace SpeedTestLogger
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SpeedTestLogger");
+
+            var config = new LoggerConfiguration();
+            var speedTestRunner = new SpeedTestRunner(config.LoggerLocation);
+
+            var testData = speedTestRunner.RunSpeedTest();
+            var results = new TestResult
+            {
+                SessionId = new Guid(),
+                User = config.UserId,
+                Device = config.LoggerId,
+                Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Data = testData
+            };
+        }
+    }
+}
